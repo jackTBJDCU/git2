de2432de0ab84eb9368eb9bfafb48bc8a7d3a835
jack johnston <jack.johnston2@mail.dcu.ie>
2026-09-18 20:15:46 +0100

    improved the legend pos

--- Comp_labs_week2_E3.py
+++ Comp_labs_week2_E3.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-h_list = [0.05, 0.01, 0.005, 0.001, 0.0005]
+h_list = [0.02, 0.01, 0.005, 0.001]
 tmax = 32
 x0, y0 = 0, 1
 
@@ -29,7 +29,7 @@ fig.set_size_inches(9, 10)
 
 T_exact_fine = np.linspace(0, tmax, 2000)
 X_exact_fine = np.sin(T_exact_fine)
-
+#plotting the Euler method using diffrent jumps 
 for h in h_list:
     t = 0.0
     x = x0
@@ -44,21 +44,20 @@ for h in h_list:
         t = t + h
         T_v.append(t)
         X_v.append(x)
-
+        
+# Exact solution
     X_exact_grid, T_exact_grid = exact_solution(h)
     X_diff = np.array(X_v) - np.array(X_exact_grid)
 
     ax1.plot(T_v, X_v, label=f"Euler h={h}")
     ax2.plot(T_v, X_diff, label=f"Euler h={h}")
 
-# Exact solution
 ax1.plot(T_exact_fine, X_exact_fine, 'k--', linewidth=2, label="exact sin(t)")
 
 ax1.set_ylabel("x")
-ax1.set_title(f"Euler method: divergence with step size (tmax={tmax})")
-ax1.legend()
+ax1.set_title(f"Euler method E3 : divergence with step size (t-max={tmax})")
+ax1.legend(loc='upper left',fontsize = 7.8)
 
-ax2.axhline(0, color='k', linewidth=0.8)
 ax2.set_xlabel("t")
 ax2.set_ylabel("x_num - x_exact")
 ax2.legend()
